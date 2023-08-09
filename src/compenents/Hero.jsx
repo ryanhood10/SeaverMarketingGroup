1dd94ecdfe5f6cbb0f6a932be32c59c033eeb26b
Ryan Hood <114943155+ryanhood10@users.noreply.github.com>
2023-08-09 16:32:39 -0500

    finished mock hero

--- src/compenents/Hero.jsx
+++ src/compenents/Hero.jsx
@@ -5,14 +5,26 @@ const Hero = () => {
 
     return (
         <div className="text-white">
-            <div className="max-w-[800px] mt-[-96px] w-full h-screem mx-auto text-center flex flex-col justify-center">
-                <p className="text-[#00df9a] font-bold p-2">REACH MORE CLIENTS</p>
-                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Grow with data.</h1>
-                <div>
-                    <p className="md:text-5xl sm:text-4xl text-xl font-bold">Engage your Customers</p>
-                    <Typed strings={['Boost Online Visibilty, Engage Clients, Find your Web Solutions']} typeSpeed={120} backSpeed={140} loop />
-                </div>
+        {/* the h-screen on the next div is what makes the height the entire screen, you can use this component for later in a smaller version as a hero */}
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+            <p className="text-[#00df9a] text-l font-bold p-2">MARKETING AND WEB SOLUTIONS <br></br>USING DATA ANALYTICS
+                </p> 
 
+                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Grow with data.
+                </h1>
+
+                <div>
+                    <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">Engage your Customers
+                    </p>
+
+                    <Typed strings={['Boost Online Visibility,', 'Engage Clients,', 'Find your Web Solutions']}
+                     typeSpeed={120}
+                      backSpeed={140}
+                      className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
+                      loop />
+                </div>
+                <p className="md:text-2xl text-xl font-bold text-gray-500 md:pl-4 pl-2">We monitor data analytics to increase your revenue, reach more customers, and keep them engaged.</p>
+                <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">Get Started</button>
             </div>
         </div>
     )
