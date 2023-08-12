f2652bdee2a6b869416d4fa42fd5783144e44fb1
Ryan Hood <114943155+ryanhood10@users.noreply.github.com>
2023-08-12 00:36:56 -0500

    added navbar styling

--- src/compenents/Header.js
+++ src/compenents/Header.js
@@ -57,7 +57,7 @@ const Header = () => {
     {/* Second menu (for side Navbar) */}
 
         <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-black ease-in-out duration-500  z-50' : "fixed left-[-100%]"}>
-                  <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Seaver Marketing Group</h1>
+                  <h1 className='w-full text-xl font-bold text-[#00df9a] m-4'>Seaver Marketing Group</h1>
 
           <ul className='pt-12 uppercase'>
         <li className='p-4 border-b border-gray-600' >
