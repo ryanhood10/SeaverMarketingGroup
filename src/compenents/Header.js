bcef23fbbc5bb1a1c3ad6709c0fb328e0780823a
Ryan Hood <114943155+ryanhood10@users.noreply.github.com>
2023-08-12 00:14:19 -0500

    fixed styling issues with navbar

--- src/compenents/Header.js
+++ src/compenents/Header.js
@@ -49,14 +49,16 @@ const Header = () => {
         </ul>
 
 
-        <div onClick={handleNav} className='block md:hidden'>
+        <div onClick={handleNav} className='block md:hidden fixed top-5 right-6 '>
           {/* if else statement to show navbar state */}
           {!nav ? <AiOutlineClose size={20}/> :    <AiOutlineMenu size={20} /> }
         </div>
 
     {/* Second menu (for side Navbar) */}
 
-        <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#00300] ease-in-out duration-500' : "fixed left-[-100%]"}>
+        <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-black ease-in-out duration-500  z-50' : "fixed left-[-100%]"}>
+                  <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Seaver Marketing Group</h1>
+
           <ul className='pt-12 uppercase'>
         <li className='p-4 border-b border-gray-600' >
             <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
--- src/compenents/OtherHero.jsx
+++ src/compenents/OtherHero.jsx
@@ -8,7 +8,7 @@ const OtherHero = () => {
         <div className="w-full mx-auto p-4">
             <div className='max-h-[500px] relative'>
                 {/* overlay */}
-                <div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
+                <div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center z-0'>
                     <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:7xl font-bold' >The <span className='text-[#00df9a]'> Best</span></h1>
                     <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:7xl font-bold' >Solutions <span className='text-[#00df9a]'> Delivered</span></h1>
                 </div>
